611f3f24dc304904f7e43c64d394092ff8d6daeb
MidasVE <r0633132@student.thomasmore.be>
2019-03-25 13:47:04 +0100

    Added dummy data + display it

--- src/app/calendar/calendar-day.component.ts
+++ src/app/calendar/calendar-day.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CalendarDayComponent implements OnInit {
+  pills = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.loadPills();
+  }
+
+  public loadPills() {
+    this.pills = [
+      {
+        name: 'prolopa',
+        dose: 1,
+        time: '8:30',
+        description: 'neem doosje 2 om 8u30'
+      },
+      {
+        name: 'azilect',
+        dose: 1,
+        time: '8:30',
+        description: 'neem doosje 2 om 8u30'
+      }
+    ];
   }
 
 }
