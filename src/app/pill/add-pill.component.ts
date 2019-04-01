8d31d87184f249079b21ff41b565413b2813d56b
MidasVE <r0633132@student.thomasmore.be>
2019-04-01 16:34:02 +0200

    Added recurrence options

--- src/app/pill/add-pill.component.ts
+++ src/app/pill/add-pill.component.ts
@@ -6,37 +6,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPillComponent implements OnInit {
 
-  public timeOfDays = [];
+  public recurrences = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.timeOfDays = [
-      { label: '0:00', value: '0:00' },
-      { label: '1:00', value: '0:00' },
-      { label: '2:00', value: '0:00' },
-      { label: '3:00', value: '0:00' },
-      { label: '4:00', value: '0:00' },
-      { label: '5:00', value: '0:00' },
-      { label: '6:00', value: '0:00' },
-      { label: '7:00', value: '0:00' },
-      { label: '8:00', value: '0:00' },
-      { label: '9:00', value: '0:00' },
-      { label: '10:00', value: '0:00' },
-      { label: '11:00', value: '0:00' },
-      { label: '12:00', value: '0:00' },
-      { label: '13:00', value: '0:00' },
-      { label: '14:00', value: '0:00' },
-      { label: '15:00', value: '0:00' },
-      { label: '16:00', value: '0:00' },
-      { label: '17:00', value: '0:00' },
-      { label: '18:00', value: '0:00' },
-      { label: '19:00', value: '0:00' },
-      { label: '20:00', value: '0:00' },
-      { label: '21:00', value: '0:00' },
-      { label: '22:00', value: '0:00' },
-      { label: '23:00', value: '0:00' },
-      { label: '24:00', value: '0:00' }
+    this.recurrences = [
+      { label: 'Elke dag', value: 'everyDay' },
+      { label: 'Elke week', value: 'everyWeek' },
+      { label: 'Elke maand', value: 'everyMonth' }
     ];
   }
 
--- src/app/pill/pill.module.ts
+++ src/app/pill/pill.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarModule } from 'primeng/calendar';
+import { AccordionModule } from 'primeng/accordion';
 
 import { AddPillComponent } from './add-pill.component';
 import { routing } from '../app.routing';
 
 @NgModule({
   declarations: [AddPillComponent],
-  imports: [CommonModule, DropdownModule, routing, BrowserAnimationsModule, CalendarModule],
+  imports: [CommonModule, DropdownModule, routing, BrowserAnimationsModule, CalendarModule, AccordionModule],
   exports: []
 })
 export class PillModule {}
