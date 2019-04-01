6969c979a18e286ddcc1b998856a782df32f70c8
MidasVE <r0633132@student.thomasmore.be>
2019-04-01 16:23:13 +0200

    Added time input & dropdown

--- src/app/pill/pill.module.ts
+++ src/app/pill/pill.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
 
 import { AddPillComponent } from './add-pill.component';
 import { routing } from '../app.routing';
 
 @NgModule({
   declarations: [AddPillComponent],
-  imports: [CommonModule, DropdownModule, routing],
+  imports: [CommonModule, DropdownModule, routing, BrowserAnimationsModule, CalendarModule],
   exports: []
 })
 export class PillModule {}
