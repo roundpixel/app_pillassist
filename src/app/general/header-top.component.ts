6d3c5120ce7cb61208890ae363915c0d6f0c573c
MidasVE <r0633132@student.thomasmore.be>
2019-04-04 15:01:15 +0200

    Added header component

---
+++ src/app/general/header-top.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-top',
+  templateUrl: './header-top.component.html',
+  styles: []
+})
+export class HeaderTopComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
