6227cbca8d3500ad78853da6e60a9a16b08d2df6
MidasVE <r0633132@student.thomasmore.be>
2019-04-10 11:46:34 +0200

    Added overlaypanel to more view

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
 import { FullCalendarModule } from 'primeng/fullcalendar';
 import { CalendarDayComponent } from './calendar-day.component';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { registerLocaleData } from '@angular/common';
 import localeNlBE from '@angular/common/locales/nl-BE';
 import { routing } from '../app.routing';
@@ -11,7 +12,7 @@ registerLocaleData(localeNlBE);
 
 @NgModule({
   declarations: [CalendarComponent, CalendarDayComponent],
-  imports: [CommonModule, FullCalendarModule, routing],
+  imports: [CommonModule, FullCalendarModule, routing, OverlayPanelModule],
   exports: [CalendarComponent, CalendarDayComponent]
 })
 export class CalendarModule {}
