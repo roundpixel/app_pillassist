463d0bb4eadc0b98afb763011511013d0bcd56ed
MidasVE <r0633132@student.thomasmore.be>
2019-04-12 10:29:03 +0200

    Added tomorrow date to component

--- src/app/calendar/calendar-day-tomorrow.component.ts
+++ src/app/calendar/calendar-day-tomorrow.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class CalendarDayTomorrowComponent implements OnInit {
+  public date = new Date();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.date.setDate(this.date.getDate() + 1);
+    const tommorow =
+      this.date.getDate() +
+      '-' +
+      (this.date.getMonth() + 1) +
+      '-' +
+      this.date.getFullYear();
+    console.log(tommorow);
   }
-
 }
