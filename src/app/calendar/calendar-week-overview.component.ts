f1a21ea4f997bac8050dbee4dd645bb1293d05eb
MidasVE <r0633132@student.thomasmore.be>
2019-04-12 15:22:25 +0200

    Changed chart styling

--- src/app/calendar/calendar-week-overview.component.ts
+++ src/app/calendar/calendar-week-overview.component.ts
@@ -7,19 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarWeekOverviewComponent implements OnInit {
   public data: any;
+  public options: any;
 
   constructor() {}
 
   ngOnInit() {
     this.data = {
-      labels: ['A', 'B', 'C'],
+      labels: ['Op tijd', 'Te laat'],
       datasets: [
         {
-          data: [300, 50, 100],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+          data: [300, 50],
+          backgroundColor: ['#35A6F5', '#52699B']
         }
       ]
+    };
+
+    this.options = {
+      cutoutPercentage: 85,
+      aspectRatio: 1,
+      legend: {
+        display: false
+      }
     };
   }
 }
