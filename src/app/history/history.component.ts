236b2346ad155be670944af19e346bee45439bb8
MidasVE <r0633132@student.thomasmore.be>
2019-06-14 10:08:09 +0200

    Changed pill history styling

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -10,13 +10,15 @@ import { PillService } from '../services/pill.service';
   styles: []
 })
 export class HistoryComponent implements OnInit {
-  public patient: Patient;
-  public pills;
   constructor(
     private route: ActivatedRoute,
     private patientService: PatientService,
     private pillService: PillService
   ) {}
+  public patient: Patient;
+  public pills;
+  timeParts;
+  [1];
 
   ngOnInit() {
     this.patientService.getAll().subscribe(patients => {
@@ -27,10 +29,30 @@ export class HistoryComponent implements OnInit {
             this.patientService.changePatient(patient);
             this.pillService.getAllPills(this.patient.id).subscribe(pills => {
               this.pills = pills;
+              this.convertDates(this.pills);
             });
           }
         });
       });
     });
+  }
+
+  public convertDates(pills) {
+    pills.forEach(pill => {
+      const timeParts = pill.time.split(/[:]/);
+      pill.time = this.convertStringToTimeString([timeParts[0], timeParts[1]]);
+
+      const [Year, Month, Day] = [...pill.date.split('-')];
+      pill.date = this.converStringToDate([Year, Month, Day]);
+    });
+  }
+
+  public convertStringToTimeString([hours, minutes]) {
+    return hours + 'u' + minutes;
+  }
+
+  public converStringToDate([year, month, day]) {
+    const monthIndex = month - 1;
+    return new Date(year, monthIndex, day);
   }
 }
